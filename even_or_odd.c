e33249b161c907366ee7e81257dc767bad7dad52
yerima192 <razacksamira8@gmail.com>
2025-07-21 03:26:39 +0100

    bb

---
+++ even_or_odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int modulo;
+
+    scanf("%d", &modulo);
+
+    if (modulo % 2)
+        printf("%d est impair.\n", modulo);
+
+    else
+        printf("%d est pair.\n", modulo);
+
+    return 0;
+}
