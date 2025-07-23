c9425a7de634c3507b265a21b1342f33fabddbc6
yerima192 <razacksamira8@gmail.com>
2025-07-23 02:20:48 +0100

    b

--- factorial.c
+++ factorial.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main()
+
 {
     int factoriel, i, n;
     factoriel = 1;
     scanf("%d",&n);
+    
     for (i = 1; i <= n; i++) {
         factoriel = factoriel * i;
     }
+
     printf("Le factoriel de %d est : %d\n", n, factoriel);
     return 0;
 }
---
+++ swap_no_temp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b );
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+     printf("Après échange: a = %d, b = %d\n", a, b );
+
+     return 0;
+}
