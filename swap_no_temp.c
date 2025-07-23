bc1b093bc4d51306b5f63a4b74573aabb28ef4a5
yerima192 <razacksamira8@gmail.com>
2025-07-23 02:50:42 +0100

    b

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,13 +2,20 @@
 
 int main()
 {
-    int a , b;
+    int a, b;
+
+    // Saisie des deux entiers
     scanf("%d %d", &a, &b);
 
+    // Affichage avant l'échange
+    printf("Avant échange: a = %d, b = %d\n", a, b);
+
+    // Échange sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
 
+    // Affichage après l'échange
     printf("Après échange: a = %d, b = %d\n", a, b);
 
     return 0;
